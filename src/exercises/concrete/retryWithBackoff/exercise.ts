99b90d05d799a761690d88b6246a1517944390ce
茂茂 <375642570@qq.com>
2023-11-29 13:48:32 +0800

    feat: Complete retryWithBackoff

--- src/exercises/concrete/retryWithBackoff/exercise.ts
+++ src/exercises/concrete/retryWithBackoff/exercise.ts
@@ -4,4 +4,19 @@ type Context = {
 };
 
 export default ({ postData, wait }: Context) =>
-  async (data: string) => {};
+  async (data: string) => {
+    let delay = 0;
+
+    while (true) {
+      try {
+        if (delay) await wait(delay);
+        return await postData(data);
+      } catch (error) {
+        if (!delay) {
+          delay = 200;
+        } else {
+          delay *= 2;
+        }
+      }
+    }
+  };
