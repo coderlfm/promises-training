77af3b745e270bad09bf3afc4640f2fe08f01201
茂茂 <375642570@qq.com>
2023-11-29 18:13:34 +0800

    feat: Complete parallelMaxConcurrency

--- src/exercises/concrete/parallelMaxConcurrency/exercise.ts
+++ src/exercises/concrete/parallelMaxConcurrency/exercise.ts
@@ -5,4 +5,32 @@ type Context = {
 };
 
 export default ({ postData }: Context) =>
-  (list: Array<string>) => {};
+  (list: Array<string>) => {
+    let runningCount = 0,
+      nextIndex = 0;
+
+    const results: Array<string> = [];
+
+    const schedule = () => {
+      if (nextIndex + 1 > list.length) return;
+
+      if (runningCount >= 5) return;
+
+      runningCount++;
+
+      const curIndex = nextIndex;
+      postData(list[curIndex]).then((res) => {
+        results[curIndex] = res;
+        runningCount--;
+        schedule();
+      });
+
+      nextIndex++;
+      schedule();
+    };
+
+    schedule();
+
+    return results;
+  };
+
