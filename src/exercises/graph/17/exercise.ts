fcc908dae1eb1afb7e22b8e4d66050f535095188
茂茂 <375642570@qq.com>
2023-12-01 11:34:41 +0800

    feat: Complete 17

--- src/exercises/graph/17/exercise.ts
+++ src/exercises/graph/17/exercise.ts
@@ -7,14 +7,44 @@ const mixed =
 
 const asyncAwait =
   ({ createPromise }: ExerciseContext) =>
-  async () => {};
+  async () => {
+    const a = createPromise("A");
+
+    const b = (async () => {
+      await a;
+      await createPromise("B");
+    })();
+
+    const c = (async () => {
+      try {
+        // await Promise.any([a, b]); // 这里不能写成 any，因为如果出现 a成功，b失败的情况。就不满足了
+        await b;
+      } catch {
+        await createPromise("C");
+      }
+    })();
+
+    const d = (async () => {
+      await b;
+      await createPromise("D");
+    })();
+
+    await Promise.all([d, c]);
+  };
 
 const thenCatch =
   ({ createPromise }: ExerciseContext) =>
-  async () => {};
+  async () => {
+    const a = createPromise("A");
+    const b = a.then(() => createPromise("B"));
+    const c = b.catch(() => createPromise("C"));
+    const d = b.then(() => createPromise("D"));
+
+    return Promise.all([c, d]);
+  };
 
 export default {
   makeMixedExercise: skipExercise(mixed),
-  makeAsyncAwaitExercise: skipExercise(asyncAwait),
-  makeThenCatchExercise: skipExercise(thenCatch),
+  makeAsyncAwaitExercise: asyncAwait,
+  makeThenCatchExercise: thenCatch,
 };
