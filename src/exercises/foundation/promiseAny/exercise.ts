f651530ad023b25eceaf7653cae6ddc6533a9d62
茂茂 <375642570@qq.com>
2023-12-01 14:42:27 +0800

    feat: Complete promiseAny

--- src/exercises/foundation/promiseAny/exercise.ts
+++ src/exercises/foundation/promiseAny/exercise.ts
@@ -1,1 +1,14 @@
-export default async <T>(promises: Array<Promise<T>>): Promise<T> => {};
+export default async <T>(promises: Array<Promise<T>>): Promise<T> => {
+  const errors: Array<string> = [];
+  let errorCount = 0;
+
+  return new Promise((resolve, reject) => {
+    promises.forEach((item, index) => {
+      item.then(resolve).catch((error) => {
+        errors[index] = error;
+        errorCount++;
+        if (errorCount === promises.length) reject(errors);
+      });
+    });
+  });
+};
