a1f47f0bbd3a6069723ec0f132175636de2b5935
茂茂 <375642570@qq.com>
2023-12-01 11:07:53 +0800

    feat: Complete 15

--- src/exercises/graph/15/exercise.ts
+++ src/exercises/graph/15/exercise.ts
@@ -7,14 +7,55 @@ const mixed =
 
 const asyncAwait =
   ({ createPromise }: ExerciseContext) =>
-  async () => {};
+  async () => {
+    const a = createPromise("A");
+    const b = createPromise("B");
+    const c = createPromise("C");
+
+    const d = (async () => {
+      await Promise.all([a, b]);
+      await createPromise("D");
+    })();
+
+    const e = (async () => {
+      await Promise.any([d, Promise.all([b, c])]);
+      await createPromise("E");
+    })();
+
+    const f = (async () => {
+      await Promise.all([b, c]);
+      await createPromise("F");
+    })();
+
+    const g = (async () => {
+      await Promise.any([b, Promise.all([a, c])]);
+      await createPromise("G");
+    })();
+
+    await Promise.all([e, f, g]);
+  };
 
 const thenCatch =
   ({ createPromise }: ExerciseContext) =>
-  async () => {};
+  async () => {
+    const a = createPromise("A");
+    const b = createPromise("B");
+    const c = createPromise("C");
+
+    const d = Promise.all([a, b]).then(() => createPromise("D"));
+    const e = Promise.any([d, Promise.all([b, c])]).then(() =>
+      createPromise("E")
+    );
+    const f = Promise.all([b, c]).then(() => createPromise("F"));
+    const g = Promise.any([b, Promise.all([a, c])]).then(() =>
+      createPromise("G")
+    );
+
+    return Promise.all([e, f, g]);
+  };
 
 export default {
   makeMixedExercise: skipExercise(mixed),
-  makeAsyncAwaitExercise: skipExercise(asyncAwait),
-  makeThenCatchExercise: skipExercise(thenCatch),
+  makeAsyncAwaitExercise: asyncAwait,
+  makeThenCatchExercise: (thenCatch),
 };
