329319a567bc2d485f00c50fe5a40c8ce7e43c67
茂茂 <375642570@qq.com>
2023-12-01 16:35:43 +0800

    feat: Complete graph 18

--- src/exercises/graph/18/exercise.ts
+++ src/exercises/graph/18/exercise.ts
@@ -7,14 +7,44 @@ const mixed =
 
 const asyncAwait =
   ({ createPromise }: ExerciseContext) =>
-  async () => {};
+  async () => {
+    const a = createPromise("A");
+
+    const b = (async () => {
+      await a;
+      await createPromise("B");
+    })();
+
+    const e = (async () => {
+      try {
+        await a;
+      } catch {
+        await Promise.all([createPromise("C"), createPromise("D")]);
+        await createPromise("E");
+      }
+    })();
+
+    await Promise.all([b, e]);
+  };
 
 const thenCatch =
   ({ createPromise }: ExerciseContext) =>
-  async () => {};
+  async () => {
+    const a = createPromise("A");
+
+    const b = a.then(() => createPromise("B"));
+
+    const e = a.catch(() =>
+      Promise.all([createPromise("C"), createPromise("D")]).then(() =>
+        createPromise("E")
+      )
+    );
+
+    return Promise.all([b, e]);
+  };
 
 export default {
   makeMixedExercise: skipExercise(mixed),
-  makeAsyncAwaitExercise: skipExercise(asyncAwait),
-  makeThenCatchExercise: skipExercise(thenCatch),
+  makeAsyncAwaitExercise: asyncAwait,
+  makeThenCatchExercise: thenCatch,
 };
