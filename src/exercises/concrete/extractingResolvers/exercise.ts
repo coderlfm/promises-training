d4af768391cce4aafa96610dc2bddca9b7f4c63c
茂茂 <375642570@qq.com>
2023-12-01 16:22:04 +0800

    feat: Complete extractingResolvers

--- src/exercises/concrete/extractingResolvers/exercise.ts
+++ src/exercises/concrete/extractingResolvers/exercise.ts
@@ -5,4 +5,25 @@ type Context = {
 };
 
 export default ({ router }: Context) =>
-  async (url: string) => {};
+  async (url: string) => {
+    return new Promise<void>((resolve, reject) => {
+      const routeChangeComplete = () => {
+        resolve();
+        cleanAll();
+      };
+      const routeChangeError = () => {
+        reject();
+        cleanAll();
+      };
+
+      const cleanAll = () => {
+        router.off("routeChangeComplete", routeChangeComplete);
+        router.off("routeChangeError", routeChangeError);
+      };
+
+      router.on("routeChangeComplete", routeChangeComplete);
+      router.on("routeChangeError", routeChangeError);
+
+      router.push(url);
+    });
+  };
