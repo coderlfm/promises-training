0814fec474d8a6699814fd735cfbe0767c9594a3
茂茂 <375642570@qq.com>
2023-12-01 13:43:13 +0800

    feat: Complete promiseAllSettled

--- src/exercises/foundation/promiseAllSettled/exercise.ts
+++ src/exercises/foundation/promiseAllSettled/exercise.ts
@@ -1,1 +1,20 @@
-export default async <T>(promises: Array<Promise<T>>) => {};
+export default async <T>(promises: Array<Promise<T>>) => {
+  const results: Array<any> = [];
+  let successCount = 0;
+
+  return new Promise((resolve) => {
+    promises.forEach((item, index) => {
+      item
+        .then((res) => {
+          results[index] = { status: "fulfilled", value: res };
+        })
+        .catch((error) => {
+          results[index] = { status: "rejected", reason: error };
+        })
+        .finally(() => {
+          successCount++;
+          if (successCount === promises.length) resolve(results);
+        });
+    });
+  });
+};
